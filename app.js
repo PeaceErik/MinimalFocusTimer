b6b88c2d473a5b2b0cc16b08946fe866c6bd3b2f
Frederik V. <103321581+PeaceErik@users.noreply.github.com>
2024-04-02 19:47:50 +0200

    Updates, added btns

--- app.js
+++ app.js
@@ -2,24 +2,22 @@
 let focusCounter = 3;
 let seconds = 60;
 let breakCounter = 2;
+userCounter = focusCounter;
 
 
 function startTimer() {
 
 
-    if (seconds >= 0) {
+    if (seconds > 0) {
         seconds --;
     } 
     else {
         seconds = 60;     
         userCounter --;
-    }
 
-    if (userCounter === 0) {
-        userCounter = breakCounter;
-    }
-    else {
-        userCounter = focusCounter;
+        if( userCounter === 0) {
+            userCounter = breakCounter;
+        }
     }
 
     document.getElementById("counter").innerHTML=`${userCounter}`;
