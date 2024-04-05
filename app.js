925e68d6fcde33bc6aca31ad50e079643d10cc9c
Frederik V. <103321581+PeaceErik@users.noreply.github.com>
2024-04-05 18:27:07 +0200

    Fixed the "switching-timer" function

--- app.js
+++ app.js
@@ -1,32 +1,48 @@
 
 document.addEventListener("DOMContentLoaded", function() {
 
-    
-let focusCounter = 3;
+let seconds = 5;
+
 let breakCounter = 2;
-let seconds = 10;
+let focusCounter = 3;
 
+let numOfSwitches = true;
 
+userCounter = focusCounter
 
 function secondsCounter() {
-
     
     if (seconds > 0) {
         seconds --;
     } else {
         userCounter --;
-        seconds = 10;
+        seconds = 5;
     }
 
     if (userCounter === 0) {
-        
+        numOfSwitches = !numOfSwitches;
+        switchCounter(numOfSwitches)
     }
 
-    console.log(seconds)
-
+    document.getElementById("counter").innerHTML = userCounter;
 }
 
-document.getElementById("counter").innerHTML = focusCounter;
+function switchCounter(numOfSwitches) {
+
+    if (numOfSwitches) {
+        userCounter = focusCounter;
+        document.getElementById("switch-state").innerHTML = "<p>It's time to focus!</p>"
+    }
+    else
+    {
+        userCounter = breakCounter;
+        document.getElementById("switch-state").innerHTML = "<p>Take a break gamle!</p>"
+    }
+    
+}
+
+
+
 setInterval(secondsCounter, 1000);
   
 })
